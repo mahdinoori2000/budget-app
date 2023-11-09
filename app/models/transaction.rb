3ae7e62aa51cf0675cfef089fd96d7926fc76c3f
Mahdi Noori <mahdinoori2389@gmail.com>
2023-11-09 11:01:04 +0430

    Add validatin for models

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,4 +3,8 @@
 class Transaction < ApplicationRecord
     belongs_to :author, class_name: 'User'
     has_and_belongs_to_many :categories  
+
+
+    validates :name, presence: true
+    validates :amount, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@
 class User < ApplicationRecord
     has_many :categories
     has_many :transactions, foreign_key: :author_id
+
+
+    validates :name, presence: true
 end
