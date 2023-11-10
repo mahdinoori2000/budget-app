a1eaa7963ff59e290e28ace35b0b4bc21207ee64
Mahdi Noori <mahdinoori2389@gmail.com>
2023-11-10 20:07:39 +0430

    Change the name of the transaction.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_09_092411) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_10_125531) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,22 +23,22 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_09_092411) do
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
-  create_table "categories_transactions", force: :cascade do |t|
+  create_table "category_payments", force: :cascade do |t|
     t.bigint "category_id", null: false
-    t.bigint "transactions_id", null: false
+    t.bigint "payment_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_categories_transactions_on_category_id"
-    t.index ["transactions_id"], name: "index_categories_transactions_on_transactions_id"
+    t.index ["category_id"], name: "index_category_payments_on_category_id"
+    t.index ["payment_id"], name: "index_category_payments_on_payment_id"
   end
 
-  create_table "transactions", force: :cascade do |t|
+  create_table "payments", force: :cascade do |t|
     t.bigint "author_id", null: false
     t.string "name"
     t.decimal "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_transactions_on_author_id"
+    t.index ["author_id"], name: "index_payments_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -55,7 +55,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_09_092411) do
   end
 
   add_foreign_key "categories", "users"
-  add_foreign_key "categories_transactions", "categories"
-  add_foreign_key "categories_transactions", "transactions", column: "transactions_id"
-  add_foreign_key "transactions", "users", column: "author_id"
+  add_foreign_key "category_payments", "categories"
+  add_foreign_key "category_payments", "payments"
+  add_foreign_key "payments", "users", column: "author_id"
 end
