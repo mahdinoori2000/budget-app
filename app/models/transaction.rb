bcf06fc901ed45700a64ceeed6225a7b9a29a96b
Mahdi Noori <mahdinoori2389@gmail.com>
2023-11-10 20:09:14 +0430

    Change the method name

--- app/models/transaction.rb
+++
@@ -1,8 +0,0 @@
-class Transaction < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  has_many :categories
-  belongs_to :category 
-
-  validates :name, presence: true
-  validates :amount, presence: true
-end
