a76c91cc257b7f6fd6264058800fc532cf1f2a44
Mahdi Noori <mahdinoori2389@gmail.com>
2023-11-11 10:18:03 +0430

    Add route for creating new payment.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 
 Rails.application.routes.draw do
   get '/categories/:category_id/payment' => 'payments#index', as: :category_payments
+  get '/categories/:category_id/payment/new' => 'payments#new', as: :new_payment
+  post '/categories/:category_id/payment' => 'payments#create', as: :create_payment
   devise_for :users
   resources :payments
   resources :categories
@@ -10,7 +12,6 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
-
   # Defines the root path route ("/")
   # root "posts#index"
   authenticated :user do
