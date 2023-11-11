4bbd012a67b2135bc10d95ee1d4630f2bd3752a3
Mahdi Noori <mahdinoori2389@gmail.com>
2023-11-11 10:17:07 +0430

    Change transaction to payment.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :categories
-  has_many :transactions, foreign_key: :author_id
+  has_many :payments, foreign_key: :author_id
 
   validates :name, presence: true
   validates :email, presence: true
