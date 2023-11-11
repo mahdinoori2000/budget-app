4609b7d5598d1ebb14605604ac05ce25348b5ddf
Mahdi Noori <mahdinoori2389@gmail.com>
2023-11-11 10:16:03 +0430

    Change has_many to has_many :through association.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   belongs_to :user
-  has_many :transactions
-
+  has_many :category_payments
+  has_many :payments, through: :category_payments
 
   validates :name, presence: true
   validates :icon, presence: true
